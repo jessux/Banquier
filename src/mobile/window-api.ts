0a3c8b8bcbf8a4658a0fbc7590ec204900a90f3a
jessux <gabriel.kahlouche@gmail.com>
2026-08-07 12:16:04 +0200

    fix: supprime le doublon d'import CategorizationProposal dans window-api

--- src/mobile/window-api.ts
+++ src/mobile/window-api.ts
@@ -18,7 +18,6 @@ import { checkForUpdates } from './updater'
 import { POWENS_CREDS, initAuth, getTempCode, getConnections, type PowensCreds } from './powens'
 import { openConnectWebview } from './powens-webview'
 import { importPowens, onProgress as onPowensProgress, emitProgress } from './powens-sync'
-import type { CategorizationProposal } from '../shared/types'
 import { getCurrentPriceEur, isMarketType, searchSymbols } from './quotes'
 import type {
   Asset,
